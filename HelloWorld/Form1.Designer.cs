a58ac3d42ed623d271974ecbade99a7b60e95aae
lucas <lucastassi@live.com>
2022-08-12 00:31:56 -0300

    First commit

---
+++ HelloWorld/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInclui = new System.Windows.Forms.Button();
+            this.btnAltera = new System.Windows.Forms.Button();
+            this.btnExclui = new System.Windows.Forms.Button();
+            this.lstRegistros = new System.Windows.Forms.ListBox();
+            this.Cidade = new System.Windows.Forms.Label();
+            this.Sexo = new System.Windows.Forms.Label();
+            this.Nome = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.checkBoxMasc = new System.Windows.Forms.CheckBox();
+            this.checkBoxFem = new System.Windows.Forms.CheckBox();
+            this.txtRegistros = new System.Windows.Forms.TextBox();
+            this.btnContarNoCtt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInclui
+            // 
+            this.btnInclui.Location = new System.Drawing.Point(300, 144);
+            this.btnInclui.Name = "btnInclui";
+            this.btnInclui.Size = new System.Drawing.Size(75, 23);
+            this.btnInclui.TabIndex = 0;
+            this.btnInclui.Text = "Inclui";
+            this.btnInclui.UseVisualStyleBackColor = true;
+            this.btnInclui.Click += new System.EventHandler(this.btnInclui_Click);
+            // 
+            // btnAltera
+            // 
+            this.btnAltera.Location = new System.Drawing.Point(381, 144);
+            this.btnAltera.Name = "btnAltera";
+            this.btnAltera.Size = new System.Drawing.Size(75, 23);
+            this.btnAltera.TabIndex = 1;
+            this.btnAltera.Text = "Altera";
+            this.btnAltera.UseVisualStyleBackColor = true;
+            this.btnAltera.Click += new System.EventHandler(this.btnAltera_Click);
+            // 
+            // btnExclui
+            // 
+            this.btnExclui.Location = new System.Drawing.Point(462, 144);
+            this.btnExclui.Name = "btnExclui";
+            this.btnExclui.Size = new System.Drawing.Size(75, 23);
+            this.btnExclui.TabIndex = 2;
+            this.btnExclui.Text = "Exclui";
+            this.btnExclui.UseVisualStyleBackColor = true;
+            this.btnExclui.Click += new System.EventHandler(this.btnExclui_Click);
+            // 
+            // lstRegistros
+            // 
+            this.lstRegistros.FormattingEnabled = true;
+            this.lstRegistros.Location = new System.Drawing.Point(34, 25);
+            this.lstRegistros.Name = "lstRegistros";
+            this.lstRegistros.Size = new System.Drawing.Size(244, 186);
+            this.lstRegistros.TabIndex = 3;
+            // 
+            // Cidade
+            // 
+            this.Cidade.AutoSize = true;
+            this.Cidade.Location = new System.Drawing.Point(335, 44);
+            this.Cidade.Name = "Cidade";
+            this.Cidade.Size = new System.Drawing.Size(40, 13);
+            this.Cidade.TabIndex = 4;
+            this.Cidade.Text = "Cidade";
+            // 
+            // Sexo
+            // 
+            this.Sexo.AutoSize = true;
+            this.Sexo.Location = new System.Drawing.Point(335, 73);
+            this.Sexo.Name = "Sexo";
+            this.Sexo.Size = new System.Drawing.Size(31, 13);
+            this.Sexo.TabIndex = 5;
+            this.Sexo.Text = "Sexo";
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(335, 105);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(35, 13);
+            this.Nome.TabIndex = 6;
+            this.Nome.Text = "Nome";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(381, 41);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(156, 20);
+            this.txtCidade.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(381, 105);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(156, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // checkBoxMasc
+            // 
+            this.checkBoxMasc.AutoSize = true;
+            this.checkBoxMasc.Location = new System.Drawing.Point(381, 73);
+            this.checkBoxMasc.Name = "checkBoxMasc";
+            this.checkBoxMasc.Size = new System.Drawing.Size(74, 17);
+            this.checkBoxMasc.TabIndex = 9;
+            this.checkBoxMasc.Text = "Masculino";
+            this.checkBoxMasc.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFem
+            // 
+            this.checkBoxFem.AutoSize = true;
+            this.checkBoxFem.Location = new System.Drawing.Point(463, 72);
+            this.checkBoxFem.Name = "checkBoxFem";
+            this.checkBoxFem.Size = new System.Drawing.Size(68, 17);
+            this.checkBoxFem.TabIndex = 10;
+            this.checkBoxFem.Text = "Feminino";
+            this.checkBoxFem.UseVisualStyleBackColor = true;
+            // 
+            // txtRegistros
+            // 
+            this.txtRegistros.Location = new System.Drawing.Point(34, 227);
+            this.txtRegistros.Multiline = true;
+            this.txtRegistros.Name = "txtRegistros";
+            this.txtRegistros.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRegistros.Size = new System.Drawing.Size(503, 116);
+            this.txtRegistros.TabIndex = 11;
+            // 
+            // btnContarNoCtt
+            // 
+            this.btnContarNoCtt.Location = new System.Drawing.Point(34, 349);
+            this.btnContarNoCtt.Name = "btnContarNoCtt";
+            this.btnContarNoCtt.Size = new System.Drawing.Size(222, 23);
+            this.btnContarNoCtt.TabIndex = 12;
+            this.btnContarNoCtt.Text = "contar no de contatos por cidade";
+            this.btnContarNoCtt.UseVisualStyleBackColor = true;
+            this.btnContarNoCtt.Click += new System.EventHandler(this.btnContarNoCtt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 391);
+            this.Controls.Add(this.btnContarNoCtt);
+            this.Controls.Add(this.txtRegistros);
+            this.Controls.Add(this.checkBoxFem);
+            this.Controls.Add(this.checkBoxMasc);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.Sexo);
+            this.Controls.Add(this.Cidade);
+            this.Controls.Add(this.lstRegistros);
+            this.Controls.Add(this.btnExclui);
+            this.Controls.Add(this.btnAltera);
+            this.Controls.Add(this.btnInclui);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInclui;
+        private System.Windows.Forms.Button btnAltera;
+        private System.Windows.Forms.Button btnExclui;
+        private System.Windows.Forms.ListBox lstRegistros;
+        private System.Windows.Forms.Label Cidade;
+        private System.Windows.Forms.Label Sexo;
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.CheckBox checkBoxMasc;
+        private System.Windows.Forms.CheckBox checkBoxFem;
+        private System.Windows.Forms.TextBox txtRegistros;
+        private System.Windows.Forms.Button btnContarNoCtt;
+    }
+}
+
---
+++ HelloWorld/Form1.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+
+        public SqlConnection cn;
+        public CultureInfo br;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.br = new CultureInfo("pt-BR");
+
+            try
+            {
+                this.cn = new SqlConnection("Data Source=DESKTOP-FJJBHMH;Initial Catalog=aueDB;Integrated Security=True");
+                this.cn.Open();
+            }
+            catch
+            {
+                Console.WriteLine("Couldnt connect to database");
+            }
+        }
+
+        private bool validCheckBox()
+        {
+            if ((checkBoxMasc.Checked && checkBoxFem.Checked) ||
+                (!checkBoxMasc.Checked && !checkBoxFem.Checked))
+            {
+                System.Windows.Forms.MessageBox.Show("Marque apenas um sexo.");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        private int checkSexo()
+        {
+            if (checkBoxMasc.Checked && !checkBoxFem.Checked)
+                return 1;
+            else
+                return 0;
+        }
+
+        private void btnInclui_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtNome.Text) && !string.IsNullOrWhiteSpace(txtCidade.Text) && validCheckBox())
+            {
+                string strCmd = "INSERT INTO registro " +
+                    "VALUES (" + "'" + txtNome.Text + "'" + ", " + checkSexo() + ", CURRENT_TIMESTAMP, " + "'" + txtCidade.Text + "'" + ");";
+                SqlCommand cmd = new SqlCommand(strCmd, this.cn);
+                cmd.ExecuteNonQuery();
+
+                lstRegistros.Items.Add("Inserido: " + txtCidade.Text + " -> " + txtNome.Text);
+            }
+        }
+
+        private void btnAltera_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtNome.Text) && !string.IsNullOrWhiteSpace(txtCidade.Text) && validCheckBox())
+            {
+                string strCmd = "UPDATE registro SET cidade = " + "'" + txtCidade.Text + "' " + "WHERE nome = " + "'" + txtNome.Text + "';";
+                SqlCommand cmd = new SqlCommand(strCmd, this.cn);
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "UPDATE registro SET sexo = " + checkSexo() + "WHERE nome = " + "'" + txtNome.Text + "';";
+                cmd.ExecuteNonQuery();
+                lstRegistros.Items.Add("Alterado: " + txtNome.Text + " -> " + txtCidade.Text);
+            }
+        }
+
+        private void btnExclui_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtNome.Text) && !string.IsNullOrWhiteSpace(txtCidade.Text))
+            {
+                string strCmd = "DELETE FROM registro WHERE nome = '" + txtNome.Text + "' AND cidade = '" + txtCidade.Text + "';";
+                SqlCommand cmd = new SqlCommand(strCmd, this.cn);
+                cmd.ExecuteNonQuery();
+                lstRegistros.Items.Add("Excluido: " + txtNome.Text + " -> " + txtCidade.Text);
+            }
+        }
+
+        private void headerRow()
+        {
+            using (SqlCommand cmd = new SqlCommand("select sum(case when sexo = 1 then 1 else 0 end) as homem," +
+                "sum(case when sexo = 0 then 1 else 0 end) as mulher " +
+                "from registro;", this.cn))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+                        txtRegistros.Text += ". Numero de contatos no banco de dados: Total: " + (Convert.ToInt32(reader["homem"]) + Convert.ToInt32(reader["mulher"]))
+                    + ", " + reader["homem"] + " homens e " + reader["mulher"] + " mulheres" + Environment.NewLine + Environment.NewLine;
+                    }
+                }
+            }
+        }
+
+        private void btnContarNoCtt_Click(object sender, EventArgs e)
+        {
+            txtRegistros.Text = "";
+            headerRow();
+
+            SqlCommand cmd = new SqlCommand("select cidade, month(data_reg) as mes," +
+                "sum(case when sexo = 1 then 1 else 0 end) as homem," +
+                "sum(case when sexo = 0 then 1 else 0 end) as mulher " +
+                "from registro " +
+                "group by cidade, month(data_reg);", this.cn);
+
+            txtRegistros.Text += "Analise dos contatos:" + Environment.NewLine + Environment.NewLine;
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                int total = 0;
+                string ultimaCidade = "";
+
+                // To help output formatting
+                bool firstRow = true;
+
+                while (reader.Read())
+                {
+                    int homem = Convert.ToInt32(reader["homem"]);
+                    int mulher = Convert.ToInt32(reader["mulher"]);
+                    int month = Convert.ToInt32(reader["mes"]);
+                    string cidade = Convert.ToString(reader["cidade"]);
+
+                    if (!String.Equals(cidade, ultimaCidade))
+                    {
+
+                        if (!firstRow)
+                            txtRegistros.Text += "Total: " + total + Environment.NewLine + Environment.NewLine;
+
+                        txtRegistros.Text += ". Contatos em " + cidade.ToUpper() + Environment.NewLine;
+                        total = 0;
+                        firstRow = false;
+                    }
+
+                    string sent = String.Format("{0}: {1}, {2} homens e {3} mulher", this.br.DateTimeFormat.GetMonthName(month), homem + mulher, homem, mulher);
+                    txtRegistros.Text += sent + Environment.NewLine;
+                    total += homem + mulher;
+                    ultimaCidade = cidade;
+                }
+
+                txtRegistros.Text += "Total: " + total + Environment.NewLine;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        ~Form1()
+        {
+            this.cn.Close();
+        }
+    }
+}
